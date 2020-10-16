cfcbcc237462eea7a9a277b7dd9fc972ff3508d3
kkrivitskaja <pratugames@gmail.com>
2020-10-16 11:06:43 +0300

    feat: impelement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+    console.log("in array:")
+    if (!Array.isArray(matrix) || matrix.length == 0) return [];
+
+    let result = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        result = result.concat(i % 2 == 1 && Array.isArray(matrix[i]) ? matrix[i].reverse() : matrix[i])
+    }
+    return result;
 }
